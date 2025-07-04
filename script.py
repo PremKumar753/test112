ebf0546792bc2ae883844a0cc8d3afdc25bc1df3
S.Prem kumar <s.premkumar@SPrems-Mac-mini.local>
2025-07-04 22:00:18 +0530

    sample code

---
+++ script.py
@@ -0,0 +1,10 @@
+import rasterio
+
+file_path = "classified_inundation.tif"
+
+with rasterio.open(file_path) as src:
+    crs = src.crs
+    if crs:
+        print("EPSG Code:", crs.to_epsg())
+    else:
+        print("No CRS information found.")
